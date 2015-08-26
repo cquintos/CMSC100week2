fa79e0ca27da78f5f073090ef267c9aab3540201
cquintos <jcarlo.quintos@gmail.com>
2015-08-26 12:23:32 +0800

    week3!

--- config/router.js
+++ config/router.js
@@ -1,15 +1,22 @@
 var student = require('./../controllers/student');
-var teacher = require('./../controllers/teacher');
+//var teacher = require('./../controllers/teacher');
 
 module.exports = function(router){
-       router.route('/student')
-             .get(student.find);
+       router.route('/students')
+             .get(student.find)
+             .post(student.insert);
+       router.route('/students/:id')
+             .get(student.findOne)
+             .put(student.update)
+             .delete(student.remove);
        return router;
+       
 };
 
+/*
 module.exports = function(router){
        router.route('/teacher')
               .get(teacher.find)
               .post(teacher.add);
        return router;
-};
+};*/
